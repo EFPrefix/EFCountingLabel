0cfc59428b813bc7bee33d29d53834e1d2dd7866
EyreFree <zheng.xu03@ele.me>
2017-10-18 11:52:17 +0800

    clean up

--- EFCountingLabel/Classes/EFCountingLabel.swift
+++ EFCountingLabel/Classes/EFCountingLabel.swift
@@ -1,7 +1,10 @@
 //
 //  EFCountingLabel.swift
+//  EFCountingLabel
 //
-//  Copyright (c) 2016 EyreFree (eyrefree@eyrefree.org)
+//  Created by EyreFree on 2016/12/11.
+//
+//  Copyright (c) 2017 EyreFree <eyrefree@eyrefree.org>
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +23,6 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
-//
 
 import UIKit
 
